cec973f59b15252538c4eb7845df2183a915ce08
UnknownAleksC <aleksanderca@getacademy.no>
2024-01-31 09:44:43 +0100

    Update

---
+++ CardGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace card_game_tools
+{
+    internal class CardGame
+    {
+        public CardGame()
+        {
+            
+        }
+    }
+}
---
+++ CardPack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace card_game_tools
+{
+    internal class CardPack
+    {
+        public int _packSize { get; private set; }
+        public CardPack()
+        {
+            _packSize = 52;
+
+        }
+
+    }
+}
